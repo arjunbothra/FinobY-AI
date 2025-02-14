496497c9db2f1bd0a869c813369e7b6d559e461e
arjunbothra <arjunbothra78@gmail.com>
2025-02-14 19:12:36 +0530

    designed home page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
+import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <Button variant='destructive'>yoyoo</Button>
+  return <div className="mt-40">
+  <HeroSection /> 
+  
+</div>
+  
 }
---
+++ components/hero.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const HeroSection = () => {
+  return (
+    <div className="pb-20 px-4">
+        <div className="container mx-auto text-center">
+            <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
+                Finoby-AI
+            </h1>
+            <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
+            AI-Driven Finance Tracking for a Wealthier Future <br />
+            Stay in Control of Your Money with Finoby AI – Track, Plan, and Grow with AI-Powered Insights!
+            </p>
+            <div className="flex justify-center space-x-4">
+                <Link href="/dashboard">
+                    <Button size="lg" className="px-8">
+                        Start Your Journey Now
+                    </Button>
+                </Link>
+                <Link href="#">
+                    <Button size="lg" variant='outline' className="px-8">
+                        Start Demo
+                    </Button>
+                </Link>
+            </div>
+            <div>
+               <div>
+                <Image 
+                src="/Tracking.jpg" 
+                width={1280}
+                height={720}
+                alt="Dashboard Preview"
+                className="max-w-[1200px] w-full h-[600px] object-cover rounded-lg shadow-2xl border mx-auto"
+                priority
+                />
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default HeroSection;
